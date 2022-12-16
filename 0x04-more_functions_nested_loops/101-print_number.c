934fa70631d008bacea5224849f96d877decb7c5
Mohamed Sboui <mohamedsboui.pro@gmail.com>
2022-12-16 14:30:27 -0800

    aze

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - main
@@ -8,12 +8,12 @@ void print_number(int n)
 {
 if (n < 0)
 {
-	putchar('-');
+	_putchar('-');
 	n = -n;
 }
 if (n >= 10)
 {
 	print_number(n / 10);
 }
-putchar(n % 10 + '0');
+_putchar(n % 10 + '0');
 }
