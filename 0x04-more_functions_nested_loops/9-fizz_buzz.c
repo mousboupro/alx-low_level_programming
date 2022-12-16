e46dd1ef983e202fc75ba7ab59ef68e4426d3591
Mohamed Sboui <mohamedsboui.pro@gmail.com>
2022-12-16 12:22:04 -0800

    aze

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,7 @@
 
 /**
  * main - s a diagonal line on the terminal
+ * Return: 0
  */
 
 int main(void)
@@ -10,23 +11,23 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-	if (i % 3 == 0 && i % 5 == 0)
-	{
-		printf("FizzBuzz ");
-	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz ");
-	}
-	else if (i % 5 == 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-		printf("%d ", i);
-	}
+if (i % 3 == 0 && i % 5 == 0)
+{
+	printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+	printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+	printf("Buzz ");
+}
+else
+{
+	printf("%d ", i);
+}
 }
 printf("\n");
-
+return (0);
 }
