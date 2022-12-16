13a4b89e3f43003679c69b86bebda77160c93f66
Mohamed Sboui <mohamedsboui.pro@gmail.com>
2022-12-16 14:06:38 -0800

    aze

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,15 +7,14 @@
 
 int main(void)
 {
-long long num = 612852475143;
-long long largest_prime_factor = 0;
-
+long num = 612852475143;
+long largest_prime_factor = 0;
+long i;
 while (num % 2 == 0)
 {
 	num /= 2;
 }
-
-for (long long i = 3; i <= sqrt(num); i += 2)
+for (i = 3; i <= sqrt(num); i += 2)
 {
 	if (num % i == 0)
 	{
@@ -29,7 +28,7 @@ if (num > 2)
 	largest_prime_factor = num;
 }
 
-printf("%lld\n", largest_prime_factor);
+printf("%ld\n", largest_prime_factor);
 
 return (0);
 }
