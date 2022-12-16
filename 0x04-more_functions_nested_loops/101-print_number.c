638c6628a244b8a800d652a009c07bbcb1eecb4e
Mohamed Sboui <mohamedsboui.pro@gmail.com>
2022-12-16 14:28:42 -0800

    aze

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_number - main
+ * @n: para
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+	putchar('-');
+	n = -n;
+}
+if (n >= 10)
+{
+	print_number(n / 10);
+}
+putchar(n % 10 + '0');
+}
