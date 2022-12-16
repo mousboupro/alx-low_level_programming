0bb9013b8d900df0268ff609b416631bebd20e2d
Mohamed Sboui <mohamedsboui.pro@gmail.com>
2022-12-16 09:47:01 -0800

    aze

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+
+
+
+#include "main.h"
+
+
+/**
+ * print_numbers - main.
+ * Return: smthing.
+ */
+
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	_putchar('0' + i);
+}
+_putchar('\n');
+}
